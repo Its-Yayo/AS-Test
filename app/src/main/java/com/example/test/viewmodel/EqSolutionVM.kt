1cedeb785043ce0651d4349a3289dbabdb95ab8b
Its-Yayo <a01754574@itesm.mx>
2023-08-28 09:53:09 -0600

    Fix Usage ViewModel #9

--- app/src/main/java/com/example/test/viewmodel/EqSolutionVM.kt
+++ app/src/main/java/com/example/test/viewmodel/EqSolutionVM.kt
@@ -1,8 +1,20 @@
 package com.example.test.viewmodel
 
+
 import androidx.lifecycle.ViewModel
+import com.example.test.model.EqSolution
+
 
 class EqSolutionVM: ViewModel() {
     // TODO Implement the ViewModel
 
+    private val model = EqSolution()
+    var root1 = 0.0
+    var root2 = 0.0
+
+    fun solve(a: Double, b: Double, c: Double) {
+        var roots = model.roots(a, b, c)
+        root1 = roots.first!!
+        root2 = roots.second!!
+    }
 }
