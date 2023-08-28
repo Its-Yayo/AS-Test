72c0a42f380585b2ee9e642028c504298259cdaf
Its-Yayo <a01754574@itesm.mx>
2023-08-28 14:50:56 -0600

    Fix Usage View FIXME #11

--- app/src/main/java/com/example/test/view/MainActivity.kt
+++ app/src/main/java/com/example/test/view/MainActivity.kt
@@ -2,15 +2,37 @@ package com.example.test.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 import com.example.test.R
+import com.example.test.databinding.ActivityMainBinding
+import com.example.test.viewmodel.EqSolutionVM
 
-class MainActivity: AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    val eqSolutionVM: EqSolutionVM by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
-        private lateinit var binding: ActivityMainBinding
-
-
         super.onCreate(savedInstanceState)
         // setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        events()
+        viewers()
+    }
+
+    private fun viewers() {
+        // FIXME 1. Subscribe to the changes of root1 and root2
+        eqSolutionVM.root1.observe(this) { value ->
+            binding.etRoot1.setText("%.2f".format(value))
+        }
+
+        eqSolutionVM.root2.observe(this) { value ->
+            binding.etRoot2.setText("%.2f".format(value))
+        }
+    }
+
+    private fun events() {
+        TODO("Not yet implemented")
     }
 }
 
