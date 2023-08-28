359c242300e157f2199074a452d1477f323c9adb
Its-Yayo <a01754574@itesm.mx>
2023-08-28 09:17:16 -0600

    Fix View, ViewModel TODO #7

--- app/src/main/java/com/example/test/model/EqSolution.kt
+++ app/src/main/java/com/example/test/model/EqSolution.kt
@@ -3,5 +3,4 @@ package com.example.test.model
 class EqSolution {
     // TODO: Implement the Model
 
-
 }
---
+++ app/src/main/java/com/example/test/viewmodel/EqSolutionVM.kt
@@ -0,0 +1,8 @@
+package com.example.test.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+class EqSolutionVM: ViewModel() {
+    // TODO Implement the ViewModel
+
+}
