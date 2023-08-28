f26d5ca385ad4e55755e765042c5b0aa4a897524
Its-Yayo <a01754574@itesm.mx>
2023-08-28 09:35:34 -0600

    Fix Usage Model #8

--- app/src/main/java/com/example/test/model/EqSolution.kt
+++ app/src/main/java/com/example/test/model/EqSolution.kt
@@ -1,6 +1,19 @@
 package com.example.test.model
 
+import kotlin.math.pow
+
 class EqSolution {
     // TODO: Implement the Model
+
+    fun roots(a: Double, b: Double, c: Double): Pair<Double?, Double?> {
+        var discriminant = b.pow(2.0) - 4 * a * c
+
+        return if (discriminant < 0) {
+            Pair(null, null)
+        } else {
+            var root1 = (-b + discriminant.pow(0.5)) / (2 * a)
+            var root2 = (-b - discriminant.pow(0.5)) / (2 * a)
+            Pair(root1, root2)
+        }
 
 }
